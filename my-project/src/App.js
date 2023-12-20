05b5ad19af8880a3544b36752896708cbb29a93b
Ze1DoS <kupeev.d@mail.ru>
2023-12-20 19:28:14 +0300

    simple navbar created

---
+++ my-project/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import HomePage from './elements/HomePage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
